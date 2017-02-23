45ad7c8bab7433d8250aa82d18bde78c49e68c18
bhargavgunreddy <gbhargavreddy@gmail.com>
2017-02-23 15:42:05 -0700

    Updating port and ip

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var expressServer = require('express');
 var app = expressServer();
 
+app.set('port', process.env.PORT || 8080);
+app.set('url', process.env.IP || 'localhost');
 
 app.use('/', function(err, res){
 	res.sendFile(__dirname+'/build/index.html');
 });
 
-app.listen(process.env.PORT, process.env.IP);
+app.listen(app.get('port'), app.get('url'));
