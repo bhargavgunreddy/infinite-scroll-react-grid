14aa9ecc08c9266507699fa4851d22bd610e56a8
bhargavgunreddy <gbhargavreddy@gmail.com>
2017-02-23 15:47:01 -0700

    commenting server

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-var expressServer = require('express');
-var app = expressServer();
+// var expressServer = require('express');
+// var app = expressServer();
 
-app.set('port', process.env.PORT || 8080);
-app.set('url', process.env.IP || 'localhost');
+// app.set('port', process.env.PORT || 8080);
+// app.set('url', process.env.IP || 'localhost');
 
-app.use('/', function(err, res){
-	res.sendFile(__dirname+'/build/index.html');
-});
+// app.use('/', function(err, res){
+// 	res.sendFile(__dirname+'/build/index.html');
+// });
 
-app.listen(app.get('port'), app.get('url'));
+// app.listen(app.get('port'), app.get('url'));
